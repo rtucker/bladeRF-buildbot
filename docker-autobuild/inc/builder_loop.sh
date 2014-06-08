6336de9b2e6d57afdde9c31363f85087ee0dbdc4
Ryan Tucker <rtucker@gmail.com>
2014-06-08 12:29:05 -0400

    add coverity upload to the main loop

--- docker-autobuild/inc/builder_loop.sh
+++ docker-autobuild/inc/builder_loop.sh
@@ -1,6 +1,7 @@
 while true
 do
     ${BINDIR}/autobuild.sh
+    ${BINDIR}/coverity_upload.sh
     ${BINDIR}/clean_builds.sh
     ${BINDIR}/update_html.sh > ${WORKDIR}/builds/index.html.new
     mv ${WORKDIR}/builds/index.html.new ${WORKDIR}/builds/index.html
